88c959f328908a69c811a8023fcc4a3da8b73f63
Peter B. Golbus <pgolbus@bu.edu>
2023-06-15 14:55:55 -0400

    be nice

--- bot.py
+++ bot.py
@@ -73,16 +73,16 @@ async def on_message(message):
             response = date_message(birthday)
             await message.channel.send(response)
         except TypeError:
-            await message.channel.send("Ew yuck I hate you!")
-            # await message.channel.send(
-            #     " ".join(
-            #         [
-            #             "I hope something good happens to you today.",
-            #             'If you tell me your birthday "@cs411-birthday-bot MM/DD/YYYY",',
-            #             "I'll tell you how many days you've been alive.",
-            #         ]
-            #     )
-            # )
+            # await message.channel.send("Ew yuck I hate you!")
+            await message.channel.send(
+                " ".join(
+                    [
+                        "I hope something good happens to you today.",
+                        'If you tell me your birthday "@cs411-birthday-bot MM/DD/YYYY",',
+                        "I'll tell you how many days you've been alive.",
+                    ]
+                )
+            )
 
 
 def days_ago(birthday):
