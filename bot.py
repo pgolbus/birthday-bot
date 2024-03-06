ca3b27328093a9c4f12aa9776b24029d95986429
Peter B. Golbus <pgolbus@gmail.com>
2024-03-06 13:40:49 -0500

    black

--- bot.py
+++ bot.py
@@ -78,10 +78,11 @@ async def on_message(message):
                     [
                         "I hope something good happens to you today.",
                         'If you tell me your birthday "@cs411-birthday-bot MM/DD/YYYY",',
-                        "I'll tell you how many days you've been alive."
+                        "I'll tell you how many days you've been alive.",
                     ]
                 )
             )
+
 
 def days_ago(birthday):
     today = date.today()
